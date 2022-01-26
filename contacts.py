0c50418892765840c8c1d4352f48309c8086507d
imsucharya <sucharya01gmail.com>
2022-01-26 10:27:28 +0530

    icon

--- contacts.py
+++ contacts.py
@@ -2,10 +2,18 @@ from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry
 from tkinter import ttk # Provides access to tk themed widgets.
 
 class contacts:
-    def __init__(self): #creating instance having a parameter self
+    def __init__(self,root): #creating instance having a parameter self
         self.root = root
+        self.CreateLeftIcon() #call the function
+
+    def CreateLeftIcon(self):
+        photo = PhotoImage(file='icons/CONTACT.gif') #creating variable
+        label = Label(image=photo)
+        label.image = photo
+        label.grid(row=0, column=0)
 
 if __name__ == '__main__':
     root = Tk()
     root.title('My Contacts List')
+    application = contacts(root)
     root.mainloop() #to run the application
