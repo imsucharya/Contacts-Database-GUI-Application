bc7418b405463240807881b9df9effca077ab47c
imsucharya <sucharya01gmail.com>
2022-01-26 09:53:22 +0530

    create instance

--- contacts.py
+++ contacts.py
@@ -1,2 +1,11 @@
 from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry, END, StringVar, Scrollbar, Toplevel
 from tkinter import ttk # Provides access to tk themed widgets.
+
+class contacts:
+    def __init__(self): #creating instance having a parameter self
+        self.root = root
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('My Contacts List')
+    root.mainloop() #to run the application
