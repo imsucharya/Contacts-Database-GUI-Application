dda5b11a9a9372bda2d9d1240b0514140eef3fb4
imsucharya <sucharya01gmail.com>
2022-01-26 09:17:11 +0530

    import

---
+++ contacts.py
@@ -0,0 +1,2 @@
+from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry, END, StringVar, Scrollbar, Toplevel
+from tkinter import ttk # Provides access to tk themed widgets.
